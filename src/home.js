08c08324c68260a7f76bec1f3d7f547c7fefce44
lmonge87 <lmonge87@gmail.com>
2021-01-24 14:49:49 -0600

    npm 5 wip

--- src/home.js
+++ src/home.js
@@ -1,40 +1,40 @@
-import React from "react";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import { GrFacebook } from "react-icons/gr";
-import { FiFacebook } from "react-icons/fi";
-import { Switch, Route, Link, useLocation } from "react-router-dom";
-import FbTools from "./pages/fbBusinessTools.js";
-import AlertManager from "./pages/alertManager.js";
+import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { GrFacebook } from 'react-icons/gr';
+import { FiFacebook } from 'react-icons/fi';
+import { Switch, Route, Link, useLocation } from 'react-router-dom';
+import FbTools from './pages/fbBusinessTools.js';
+import AlertManager from './pages/alertManager.js';
 
 export default function Homepage() {
   const currentLocation = useLocation();
 
   return (
     <>
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg='dark' variant='dark'>
         <Navbar.Brand>
-          <Link to="/">
+          <Link to='/'>
             <GrFacebook />
           </Link>
         </Navbar.Brand>
-        <Nav className="ml-auto">
-          <Link className="nav-link" to="businessTools">
+        <Nav className='ml-auto'>
+          <Link className='nav-link' to='businessTools'>
             Business Tools
           </Link>
-          <Link className="nav-link" to="alertManager">
+          <Link className='nav-link' to='alertManager'>
             Alert Manager
           </Link>
         </Nav>
       </Navbar>
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <Home />
         </Route>
-        <Route path="/businessTools">
+        <Route path='/businessTools'>
           <FbTools currentLocation={currentLocation.pathname} />
         </Route>
-        <Route path="/alertManager">
+        <Route path='/alertManager'>
           <AlertManager />
         </Route>
       </Switch>
@@ -43,5 +43,5 @@ export default function Homepage() {
 }
 
 const Home = () => {
-  return <FiFacebook className="mainLogo" />;
+  return <FiFacebook className='mainLogo' />;
 };
